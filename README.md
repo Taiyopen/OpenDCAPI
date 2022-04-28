281b6c336f3a56d147489759dbf552cae787d612
Taiyopen <jeff900418@gmail.com>
2022-04-29 03:28:31 +0800

    delete musicAPI(because ikuho has been update a new version for musicAPI and order function

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # OpenDCAPI
 An API for discord js
+此為Taiyopen設計的Discord js API
+未來將加入ikubot中
+
+
+## 當前功能
+
+### openAPI
+核心API，未來在discord.js中只需call此類別即可
+
+### roleAPI
+身分組API
+
+### jsonAPI
+讀取json的API
+
+### openAPI.json
+設定檔
+
+### musicAPI
+音樂播放API
--- openAPI.js
+++ openAPI.js
@@ -1,6 +1,6 @@
 const { jsonAPI } = require("./jsonAPI");
 const { roleAPI } = require("./roleAPI");
-const { musicAPI} = require("./musicAPI");
+const { musicAPI } = require("./musicAPI");
 const { prefix } = require('../config.json');
 
 class openAPI {
@@ -9,7 +9,7 @@ class openAPI {
         data = jsonAPI.searchJSON(data, 'server');
         const server = data;
         this.serverRegister(member.guild);
-        for (var i in server) {           
+        for (var i in server) {
             if ((member.guild.id == server[i].server_ID)) {
                 data = jsonAPI.searchJSON(data, i)
                 data = jsonAPI.searchJSON(data, 'emoji_to_role');
@@ -23,7 +23,7 @@ class openAPI {
         var data = jsonAPI.importJSON('./openAPI.json');
         data = jsonAPI.searchJSON(data, 'server');
         const server = data;
-        for (var i in server) {           
+        for (var i in server) {
             if ((member.guild.id == server[i].server_ID)) {
                 data = jsonAPI.searchJSON(data, i)
                 data = jsonAPI.searchJSON(data, 'emoji_to_role');
@@ -34,11 +34,11 @@ class openAPI {
         }
     }
 
-    static serverRegister(server){
+    static serverRegister(server) {
         var data = jsonAPI.importJSON('./openAPI.json');
         console.log(server);
-        for(var i in data.server){
-            if(server.id == data.server[i].server_ID){
+        for (var i in data.server) {
+            if (server.id == data.server[i].server_ID) {
                 return console.log('server id is existed');
             }
         }
@@ -50,92 +50,7 @@ class openAPI {
             "emoji_to_role": []
         }
         data.server.push(newdata);
-        jsonAPI.writeJSON(data, './OpenDCAPI/openAPI.json');   
-    }
-
-    static async musicON(msg){
-    if (!msg.guild) return;
-
-    const music = new musicAPI();
-    // !!join
-    if (msg.content === `${prefix}join`) {
-
-        // 機器人加入語音頻道
-        music.join(msg);
-    }
-
-    // 如果使用者輸入的內容中包含 !!play 或是 p
-    if (msg.content.indexOf(`${prefix}play`) > -1) {
-
-        // 如果使用者在語音頻道中
-        if (msg.member.voice.channel) {
-
-            // 播放音樂
-            await music.play(msg);
-        } else {
-
-            // 如果使用者不在任何一個語音頻道
-            msg.reply('你必須先加入語音頻道');
-        }
-    }
-/*
-    if (msg.content.indexOf(`${prefix}p`) > -1 ) {
-
-        // 如果使用者在語音頻道中
-        if (msg.member.voice.channel) {
-
-            // 播放音樂
-            await music.play(msg);
-        } else {
-
-            // 如果使用者不在任何一個語音頻道
-            msg.reply('你必須先加入語音頻道');
-        }
-    }
-*/
-
-    // !!resume
-    if (msg.content === `${prefix}resume`) {
-
-        // 恢復音樂
-        music.resume(msg);
-    }
-
-    // !!repeat or rp
-    if (msg.content === `${prefix}repeat`) {
-
-        // 重複音樂
-        music.repeat(msg);
-    }
-
-
-    // !!pause
-    if (msg.content === `${prefix}pause`) {
-
-        // 暫停音樂
-        music.pause(msg);
-    }
-
-    // !!skip
-    if (msg.content === `${prefix}skip`) {
-
-        // 跳過音樂
-        music.skip(msg);
-    }
-
-    // !!queue
-    if (msg.content === `${prefix}queue`) {
-
-        // 查看隊列
-        music.nowQueue(msg);
-    }
-
-    // !!leave
-    if (msg.content === `${prefix}leave`) {
-
-        // 機器人離開頻道
-        music.leave(msg);
-    }
+        jsonAPI.writeJSON(data, './OpenDCAPI/openAPI.json');
     }
 }
 module.exports = { openAPI }
